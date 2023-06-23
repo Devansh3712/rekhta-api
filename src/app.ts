b8a3da5df192987173a3c474dab16ca8aceb1c03
Devansh3712 <devanshamity@gmail.com>
2023-06-24 00:16:20 +0530

    Create ghazal route
    Signed-off-by: Devansh3712 <devanshamity@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import express, { Application, Request, Response } from 'express';
 
+import ghazal from './routes/ghazal.js';
 import word from './routes/word.js';
 
 const app: Application = express();
 
 const PORT: string | number = process.env.PORT || 8000;
 
+app.use('/ghazal', ghazal);
 app.use('/word-of-the-day', word);
 app.use('/', (req: Request, res: Response): void => {
-	res.status(404).json({ error: 'Endpoint not found' });
+	res.status(404).json({ message: 'Endpoint not found' });
 });
 
 app.listen(PORT, (): void => {
--- src/routes/word.ts
+++ src/routes/word.ts
@@ -4,10 +4,13 @@ import { getWordOfTheDay } from 'rekhta';
 const router: Router = express.Router();
 
 router.get('/', async (req: Request, res: Response): Promise<Response> => {
-	const date: string =
-		req.params?.date || new Date().toISOString().slice(0, 10);
-	const word = await getWordOfTheDay(date);
-	return res.status(200).json(word);
+	const date = req.params?.date;
+	try {
+		const word = await getWordOfTheDay(date);
+		return res.status(200).json(word);
+	} catch (err: any) {
+		return res.status(400).json({ message: err.toString() });
+	}
 });
 
 export default router;
